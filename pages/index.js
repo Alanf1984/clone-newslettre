6d9cb4af5cfb2cf84e7e1d601d2719d0c5b221d5
Alanf1984 <62482245+Alanf1984@users.noreply.github.com>
2024-03-24 23:40:05 +0000

    alterando a msg2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mel o pai te ama! Se essa mensagem chegou até vc de uma risadinha</h1            >
+    return <h1>Mel, o pai te ama! Se essa mensagem chegou até vc é sinal que fiz o exercicio certo então de uma risadinha</h1            >
 }
 
 export default Home;
